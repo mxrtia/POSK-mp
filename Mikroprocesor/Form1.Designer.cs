142e26bd874d5d477f5a3d77b0ca04162768fab1
mxrtia <41866138+mxrtia@users.noreply.github.com>
2019-12-11 23:39:02 +0100

    AX BX CX DX

--- Mikroprocesor/Form1.Designer.cs
+++ Mikroprocesor/Form1.Designer.cs
@@ -28,13 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AX = new System.Windows.Forms.Label();
+            this.BX = new System.Windows.Forms.Label();
+            this.CX = new System.Windows.Forms.Label();
+            this.DX = new System.Windows.Forms.Label();
+            this.NH = new System.Windows.Forms.Label();
+            this.NL = new System.Windows.Forms.Label();
+            this.register1 = new System.Windows.Forms.TextBox();
+            this.register2 = new System.Windows.Forms.TextBox();
+            this.register3 = new System.Windows.Forms.TextBox();
+            this.register4 = new System.Windows.Forms.TextBox();
+            this.of1 = new System.Windows.Forms.TextBox();
+            this.of2 = new System.Windows.Forms.TextBox();
+            this.of3 = new System.Windows.Forms.TextBox();
+            this.of4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // AX
+            // 
+            this.AX.AutoSize = true;
+            this.AX.Location = new System.Drawing.Point(413, 253);
+            this.AX.Name = "AX";
+            this.AX.Size = new System.Drawing.Size(26, 17);
+            this.AX.TabIndex = 0;
+            this.AX.Text = "AX";
+            // 
+            // BX
+            // 
+            this.BX.AutoSize = true;
+            this.BX.Location = new System.Drawing.Point(413, 294);
+            this.BX.Name = "BX";
+            this.BX.Size = new System.Drawing.Size(26, 17);
+            this.BX.TabIndex = 1;
+            this.BX.Text = "BX";
+            // 
+            // CX
+            // 
+            this.CX.AutoSize = true;
+            this.CX.Location = new System.Drawing.Point(413, 332);
+            this.CX.Name = "CX";
+            this.CX.Size = new System.Drawing.Size(26, 17);
+            this.CX.TabIndex = 2;
+            this.CX.Text = "CX";
+            // 
+            // DX
+            // 
+            this.DX.AutoSize = true;
+            this.DX.Location = new System.Drawing.Point(412, 373);
+            this.DX.Name = "DX";
+            this.DX.Size = new System.Drawing.Size(27, 17);
+            this.DX.TabIndex = 3;
+            this.DX.Text = "DX";
+            // 
+            // NH
+            // 
+            this.NH.AutoSize = true;
+            this.NH.Location = new System.Drawing.Point(543, 218);
+            this.NH.Name = "NH";
+            this.NH.Size = new System.Drawing.Size(28, 17);
+            this.NH.TabIndex = 4;
+            this.NH.Text = "NH";
+            // 
+            // NL
+            // 
+            this.NL.AutoSize = true;
+            this.NL.Location = new System.Drawing.Point(634, 218);
+            this.NL.Name = "NL";
+            this.NL.Size = new System.Drawing.Size(26, 17);
+            this.NL.TabIndex = 5;
+            this.NL.Text = "NL";
+            // 
+            // register1
+            // 
+            this.register1.Location = new System.Drawing.Point(504, 253);
+            this.register1.Name = "register1";
+            this.register1.ReadOnly = true;
+            this.register1.Size = new System.Drawing.Size(187, 22);
+            this.register1.TabIndex = 6;
+            // 
+            // register2
+            // 
+            this.register2.Location = new System.Drawing.Point(504, 294);
+            this.register2.Name = "register2";
+            this.register2.ReadOnly = true;
+            this.register2.Size = new System.Drawing.Size(187, 22);
+            this.register2.TabIndex = 7;
+            // 
+            // register3
+            // 
+            this.register3.Location = new System.Drawing.Point(504, 332);
+            this.register3.Name = "register3";
+            this.register3.ReadOnly = true;
+            this.register3.Size = new System.Drawing.Size(187, 22);
+            this.register3.TabIndex = 8;
+            // 
+            // register4
+            // 
+            this.register4.Location = new System.Drawing.Point(504, 370);
+            this.register4.Name = "register4";
+            this.register4.ReadOnly = true;
+            this.register4.Size = new System.Drawing.Size(187, 22);
+            this.register4.TabIndex = 9;
+            // 
+            // of1
+            // 
+            this.of1.Location = new System.Drawing.Point(459, 253);
+            this.of1.Name = "of1";
+            this.of1.ReadOnly = true;
+            this.of1.Size = new System.Drawing.Size(39, 22);
+            this.of1.TabIndex = 10;
+            // 
+            // of2
+            // 
+            this.of2.Location = new System.Drawing.Point(459, 294);
+            this.of2.Name = "of2";
+            this.of2.ReadOnly = true;
+            this.of2.Size = new System.Drawing.Size(39, 22);
+            this.of2.TabIndex = 11;
+            // 
+            // of3
+            // 
+            this.of3.Location = new System.Drawing.Point(459, 332);
+            this.of3.Name = "of3";
+            this.of3.ReadOnly = true;
+            this.of3.Size = new System.Drawing.Size(39, 22);
+            this.of3.TabIndex = 12;
+            // 
+            // of4
+            // 
+            this.of4.Location = new System.Drawing.Point(459, 370);
+            this.of4.Name = "of4";
+            this.of4.ReadOnly = true;
+            this.of4.Size = new System.Drawing.Size(39, 22);
+            this.of4.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.of4);
+            this.Controls.Add(this.of3);
+            this.Controls.Add(this.of2);
+            this.Controls.Add(this.of1);
+            this.Controls.Add(this.register4);
+            this.Controls.Add(this.register3);
+            this.Controls.Add(this.register2);
+            this.Controls.Add(this.register1);
+            this.Controls.Add(this.NL);
+            this.Controls.Add(this.NH);
+            this.Controls.Add(this.DX);
+            this.Controls.Add(this.CX);
+            this.Controls.Add(this.BX);
+            this.Controls.Add(this.AX);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label AX;
+        private System.Windows.Forms.Label BX;
+        private System.Windows.Forms.Label CX;
+        private System.Windows.Forms.Label DX;
+        private System.Windows.Forms.Label NH;
+        private System.Windows.Forms.Label NL;
+        private System.Windows.Forms.TextBox register1;
+        private System.Windows.Forms.TextBox register2;
+        private System.Windows.Forms.TextBox register3;
+        private System.Windows.Forms.TextBox register4;
+        private System.Windows.Forms.TextBox of1;
+        private System.Windows.Forms.TextBox of2;
+        private System.Windows.Forms.TextBox of3;
+        private System.Windows.Forms.TextBox of4;
     }
 }
 
