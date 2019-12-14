8935e21cb90974add25e8909e035624093685217
mxrtia <41866138+mxrtia@users.noreply.github.com>
2019-12-14 23:23:32 +0100

    Form ready

--- Mikroprocesor/Form1.Designer.cs
+++ Mikroprocesor/Form1.Designer.cs
@@ -42,12 +42,34 @@
             this.of2 = new System.Windows.Forms.TextBox();
             this.of3 = new System.Windows.Forms.TextBox();
             this.of4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProgramText = new System.Windows.Forms.TextBox();
+            this.ZerowanieRejestrow = new System.Windows.Forms.Button();
+            this.OF = new System.Windows.Forms.Label();
+            this.ListaRozkazow = new System.Windows.Forms.Label();
+            this.register1b = new System.Windows.Forms.TextBox();
+            this.register2b = new System.Windows.Forms.TextBox();
+            this.register3b = new System.Windows.Forms.TextBox();
+            this.register4b = new System.Windows.Forms.TextBox();
+            this.WyborRejestru = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // AX
             // 
             this.AX.AutoSize = true;
-            this.AX.Location = new System.Drawing.Point(413, 253);
+            this.AX.Location = new System.Drawing.Point(275, 65);
             this.AX.Name = "AX";
             this.AX.Size = new System.Drawing.Size(26, 17);
             this.AX.TabIndex = 0;
@@ -56,7 +78,7 @@
             // BX
             // 
             this.BX.AutoSize = true;
-            this.BX.Location = new System.Drawing.Point(413, 294);
+            this.BX.Location = new System.Drawing.Point(275, 93);
             this.BX.Name = "BX";
             this.BX.Size = new System.Drawing.Size(26, 17);
             this.BX.TabIndex = 1;
@@ -65,7 +87,7 @@
             // CX
             // 
             this.CX.AutoSize = true;
-            this.CX.Location = new System.Drawing.Point(413, 332);
+            this.CX.Location = new System.Drawing.Point(274, 121);
             this.CX.Name = "CX";
             this.CX.Size = new System.Drawing.Size(26, 17);
             this.CX.TabIndex = 2;
@@ -74,7 +96,7 @@
             // DX
             // 
             this.DX.AutoSize = true;
-            this.DX.Location = new System.Drawing.Point(412, 373);
+            this.DX.Location = new System.Drawing.Point(273, 152);
             this.DX.Name = "DX";
             this.DX.Size = new System.Drawing.Size(27, 17);
             this.DX.TabIndex = 3;
@@ -83,7 +105,7 @@
             // NH
             // 
             this.NH.AutoSize = true;
-            this.NH.Location = new System.Drawing.Point(543, 218);
+            this.NH.Location = new System.Drawing.Point(405, 35);
             this.NH.Name = "NH";
             this.NH.Size = new System.Drawing.Size(28, 17);
             this.NH.TabIndex = 4;
@@ -92,7 +114,7 @@
             // NL
             // 
             this.NL.AutoSize = true;
-            this.NL.Location = new System.Drawing.Point(634, 218);
+            this.NL.Location = new System.Drawing.Point(496, 35);
             this.NL.Name = "NL";
             this.NL.Size = new System.Drawing.Size(26, 17);
             this.NL.TabIndex = 5;
@@ -100,39 +122,44 @@
             // 
             // register1
             // 
-            this.register1.Location = new System.Drawing.Point(504, 253);
+            this.register1.Location = new System.Drawing.Point(366, 65);
+            this.register1.MaxLength = 8;
             this.register1.Name = "register1";
             this.register1.ReadOnly = true;
-            this.register1.Size = new System.Drawing.Size(187, 22);
+            this.register1.Size = new System.Drawing.Size(91, 22);
             this.register1.TabIndex = 6;
             // 
             // register2
             // 
-            this.register2.Location = new System.Drawing.Point(504, 294);
+            this.register2.Location = new System.Drawing.Point(366, 93);
+            this.register2.MaxLength = 8;
             this.register2.Name = "register2";
             this.register2.ReadOnly = true;
-            this.register2.Size = new System.Drawing.Size(187, 22);
+            this.register2.Size = new System.Drawing.Size(91, 22);
             this.register2.TabIndex = 7;
             // 
             // register3
             // 
-            this.register3.Location = new System.Drawing.Point(504, 332);
+            this.register3.Location = new System.Drawing.Point(365, 121);
+            this.register3.MaxLength = 8;
             this.register3.Name = "register3";
             this.register3.ReadOnly = true;
-            this.register3.Size = new System.Drawing.Size(187, 22);
+            this.register3.Size = new System.Drawing.Size(91, 22);
             this.register3.TabIndex = 8;
             // 
             // register4
             // 
-            this.register4.Location = new System.Drawing.Point(504, 370);
+            this.register4.Location = new System.Drawing.Point(365, 149);
+            this.register4.MaxLength = 8;
             this.register4.Name = "register4";
             this.register4.ReadOnly = true;
-            this.register4.Size = new System.Drawing.Size(187, 22);
+            this.register4.Size = new System.Drawing.Size(91, 22);
             this.register4.TabIndex = 9;
             // 
             // of1
             // 
-            this.of1.Location = new System.Drawing.Point(459, 253);
+            this.of1.Location = new System.Drawing.Point(321, 65);
+            this.of1.MaxLength = 1;
             this.of1.Name = "of1";
             this.of1.ReadOnly = true;
             this.of1.Size = new System.Drawing.Size(39, 22);
@@ -140,7 +167,8 @@
             // 
             // of2
             // 
-            this.of2.Location = new System.Drawing.Point(459, 294);
+            this.of2.Location = new System.Drawing.Point(321, 93);
+            this.of2.MaxLength = 1;
             this.of2.Name = "of2";
             this.of2.ReadOnly = true;
             this.of2.Size = new System.Drawing.Size(39, 22);
@@ -148,7 +176,8 @@
             // 
             // of3
             // 
-            this.of3.Location = new System.Drawing.Point(459, 332);
+            this.of3.Location = new System.Drawing.Point(320, 121);
+            this.of3.MaxLength = 1;
             this.of3.Name = "of3";
             this.of3.ReadOnly = true;
             this.of3.Size = new System.Drawing.Size(39, 22);
@@ -156,17 +185,246 @@
             // 
             // of4
             // 
-            this.of4.Location = new System.Drawing.Point(459, 370);
+            this.of4.Location = new System.Drawing.Point(320, 149);
+            this.of4.MaxLength = 1;
             this.of4.Name = "of4";
             this.of4.ReadOnly = true;
             this.of4.Size = new System.Drawing.Size(39, 22);
             this.of4.TabIndex = 13;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(59, 133);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nowy rozkaz:";
+            // 
+            // ProgramText
+            // 
+            this.ProgramText.AcceptsReturn = true;
+            this.ProgramText.AcceptsTab = true;
+            this.ProgramText.Location = new System.Drawing.Point(365, 226);
+            this.ProgramText.Multiline = true;
+            this.ProgramText.Name = "ProgramText";
+            this.ProgramText.ReadOnly = true;
+            this.ProgramText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ProgramText.Size = new System.Drawing.Size(198, 109);
+            this.ProgramText.TabIndex = 45;
+            // 
+            // ZerowanieRejestrow
+            // 
+            this.ZerowanieRejestrow.Location = new System.Drawing.Point(379, 177);
+            this.ZerowanieRejestrow.Name = "ZerowanieRejestrow";
+            this.ZerowanieRejestrow.Size = new System.Drawing.Size(161, 32);
+            this.ZerowanieRejestrow.TabIndex = 46;
+            this.ZerowanieRejestrow.Text = "Zerowanie rejestrów";
+            this.ZerowanieRejestrow.UseVisualStyleBackColor = true;
+            // 
+            // OF
+            // 
+            this.OF.AutoSize = true;
+            this.OF.Location = new System.Drawing.Point(333, 35);
+            this.OF.Name = "OF";
+            this.OF.Size = new System.Drawing.Size(27, 17);
+            this.OF.TabIndex = 47;
+            this.OF.Text = "OF";
+            // 
+            // ListaRozkazow
+            // 
+            this.ListaRozkazow.AutoSize = true;
+            this.ListaRozkazow.Location = new System.Drawing.Point(254, 241);
+            this.ListaRozkazow.Name = "ListaRozkazow";
+            this.ListaRozkazow.Size = new System.Drawing.Size(105, 17);
+            this.ListaRozkazow.TabIndex = 48;
+            this.ListaRozkazow.Text = "Lista rozkazów:";
+            // 
+            // register1b
+            // 
+            this.register1b.Location = new System.Drawing.Point(463, 65);
+            this.register1b.MaxLength = 8;
+            this.register1b.Name = "register1b";
+            this.register1b.ReadOnly = true;
+            this.register1b.Size = new System.Drawing.Size(100, 22);
+            this.register1b.TabIndex = 49;
+            // 
+            // register2b
+            // 
+            this.register2b.Location = new System.Drawing.Point(463, 93);
+            this.register2b.MaxLength = 8;
+            this.register2b.Name = "register2b";
+            this.register2b.ReadOnly = true;
+            this.register2b.Size = new System.Drawing.Size(100, 22);
+            this.register2b.TabIndex = 50;
+            // 
+            // register3b
+            // 
+            this.register3b.Location = new System.Drawing.Point(463, 121);
+            this.register3b.MaxLength = 8;
+            this.register3b.Name = "register3b";
+            this.register3b.ReadOnly = true;
+            this.register3b.Size = new System.Drawing.Size(100, 22);
+            this.register3b.TabIndex = 51;
+            // 
+            // register4b
+            // 
+            this.register4b.Location = new System.Drawing.Point(463, 149);
+            this.register4b.MaxLength = 8;
+            this.register4b.Name = "register4b";
+            this.register4b.ReadOnly = true;
+            this.register4b.Size = new System.Drawing.Size(100, 22);
+            this.register4b.TabIndex = 52;
+            // 
+            // WyborRejestru
+            // 
+            this.WyborRejestru.AutoSize = true;
+            this.WyborRejestru.Location = new System.Drawing.Point(56, 113);
+            this.WyborRejestru.Name = "WyborRejestru";
+            this.WyborRejestru.Size = new System.Drawing.Size(105, 17);
+            this.WyborRejestru.TabIndex = 53;
+            this.WyborRejestru.Text = "Wybór rejestru:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(59, 199);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 54;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 229);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.TabIndex = 55;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(36, 201);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(17, 16);
+            this.radioButton1.TabIndex = 56;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(36, 229);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(17, 16);
+            this.radioButton2.TabIndex = 57;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 58;
+            this.button1.Text = "Dodaj rozkaz";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 17);
+            this.label2.TabIndex = 59;
+            this.label2.Text = "Argument/Stała";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(300, 368);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(190, 21);
+            this.radioButton3.TabIndex = 61;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Tryb adresowy rejestrowy";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(300, 395);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(225, 21);
+            this.radioButton4.TabIndex = 62;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Tryb adresowy natychmiastowy";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(59, 368);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(216, 21);
+            this.radioButton5.TabIndex = 63;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Tryb całościowego wykonania";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(59, 395);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(156, 21);
+            this.radioButton6.TabIndex = 64;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Tryb pracy krokowej";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(628, 442);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.WyborRejestru);
+            this.Controls.Add(this.register4b);
+            this.Controls.Add(this.register3b);
+            this.Controls.Add(this.register2b);
+            this.Controls.Add(this.register1b);
+            this.Controls.Add(this.ListaRozkazow);
+            this.Controls.Add(this.OF);
+            this.Controls.Add(this.ZerowanieRejestrow);
+            this.Controls.Add(this.ProgramText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.of4);
             this.Controls.Add(this.of3);
             this.Controls.Add(this.of2);
@@ -204,6 +462,28 @@
         private System.Windows.Forms.TextBox of2;
         private System.Windows.Forms.TextBox of3;
         private System.Windows.Forms.TextBox of4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ProgramText;
+        private System.Windows.Forms.Button ZerowanieRejestrow;
+        private System.Windows.Forms.Label OF;
+        private System.Windows.Forms.Label ListaRozkazow;
+        private System.Windows.Forms.TextBox register1b;
+        private System.Windows.Forms.TextBox register2b;
+        private System.Windows.Forms.TextBox register3b;
+        private System.Windows.Forms.TextBox register4b;
+        private System.Windows.Forms.Label WyborRejestru;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
     }
 }
 
